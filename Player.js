2930c26a95ad7b77e0a52fb782fd00344ba04422
Earl Duque <eduque@ucdavis.edu>
2018-04-09 15:59:58 -0700

    added speech functionality. moved variables to objects.

--- Player.js
+++ Player.js
@@ -1,45 +1,60 @@
 (function executeRule(current, previous /*null when async*/) {
 
     var validDirections = ['face', 'turn', 'look'];
+    var validSpeech = ['say', 'whisper', 'scream', 'shout'];
 
-    var already_facing = 'You are already facing that direction';
-    var new_facing = 'You are now facing';
-    var bad_direction = 'Face where? North? Left? Up? I do not know of';
-    var you_turned = 'You turned';
-    var spin_around = 'You spin wildly but you are still facing';
+    //directions
+    var directions = {
+        already_facing: 'You are already facing that direction',
+        new_facing: 'You are now facing',
+        bad_direction: 'What do you mean? I do not know of',
+        you_turned: 'You turned',
+        spin_around: 'You spin wildly but you are still facing'
+    };
+    //speech
+    var speech = {
+        no_one_responds: ' something into the air but no one responds.',
+    };
+    //responses
+    var responses = {
+        "hello": 'Hello there :)',
+        "help me": "You're smart, you can figure it out.",
+        "how are you": "Quite well, thank you.",
+        "help": "No.",
+        "howdy": "There's a snake in my boot!"
+    };
 
     var newComment = current.comments.getJournalEntry(1).split('\n');
-    var newInput = newComment[1].split(' ');
+    var newCommand = newComment[1].split(' ');
+    var newInput = newComment[1].toString().slice(newCommand[0].length + 1).replace(/\b[-.,()&$?#!\[\]{}"']+\B|\B[-.,()&$#!\[\]{}"']+\b/g, "");
     var adminComment = '';
 
-    if (validDirections.indexOf(newInput[0]) > -1) {
-        if (current.u_direction == newInput[1]) {
-            buildComment(already_facing);
-        } else if ((current.u_direction == 'up' || current.u_direction == 'down') && (newInput[1] == 'left' || newInput[1] == 'right')) {
-            buildComment(spin_around, current.u_direction.toString());
+    if (validDirections.indexOf(newCommand[0]) > -1) {
+        if (current.u_direction == newCommand[1]) {
+            buildComment(directions.already_facing);
+        } else if ((current.u_direction == 'up' || current.u_direction == 'down') && (newCommand[1] == 'left' || newCommand[1] == 'right')) {
+            buildComment(directions.spin_around, current.u_direction.toString());
         } else {
-            switch (newInput[1]) {
-                case 'north': current.u_direction = 'north'; buildComment(new_facing, newInput[1]); break;
-                case 'east': current.u_direction = 'east'; buildComment(new_facing, newInput[1]); break;
-                case 'south': current.u_direction = 'south'; buildComment(new_facing, newInput[1]); break;
-                case 'west': current.u_direction = 'west'; buildComment(new_facing, newInput[1]); break;
-                case 'up': current.u_direction = 'up'; buildComment(new_facing, newInput[1]); break;
-                case 'down': current.u_direction = 'down'; buildComment(new_facing, newInput[1]); break;
-                case 'left': current.u_direction = newCompassDirection(current.u_direction.toString(), 'left'); buildComment(you_turned, newInput[1]); break;
-                case 'right': current.u_direction = newCompassDirection(current.u_direction.toString(), 'right'); buildComment(you_turned, newInput[1]); break;
-                default: buildComment(bad_direction, newInput[1]);
+            switch (newCommand[1]) {
+                case 'north': current.u_direction = 'north'; buildComment(directions.new_facing, newCommand[1]); break;
+                case 'east': current.u_direction = 'east'; buildComment(directions.new_facing, newCommand[1]); break;
+                case 'south': current.u_direction = 'south'; buildComment(directions.new_facing, newCommand[1]); break;
+                case 'west': current.u_direction = 'west'; buildComment(directions.new_facing, newCommand[1]); break;
+                case 'up': current.u_direction = 'up'; buildComment(directions.new_facing, newCommand[1]); break;
+                case 'down': current.u_direction = 'down'; buildComment(directions.new_facing, newCommand[1]); break;
+                case 'left': current.u_direction = newCompassDirection(current.u_direction.toString(), 'left'); buildComment(directions.you_turned, newCommand[1]); break;
+                case 'right': current.u_direction = newCompassDirection(current.u_direction.toString(), 'right'); buildComment(directions.you_turned, newCommand[1]); break;
+                default: buildComment(directions.bad_direction, newCommand[1]);
             }
         }
-
-        var currentID = current.sys_id;
-
-        current.update();
-
-        var gr2 = new GlideRecord('u_404_adventure');
-        gr2.get(currentID);
-        gr2.comments.setJournalEntry(adminComment, 'admin');
-        gr2.update();
-
+    } else if (validSpeech.indexOf(newCommand[0]) > -1) {
+        if (newInput in responses) {
+            buildComment(responses[newInput] + '');
+        } else if (newCommand[1]) {
+            buildComment('You ' + newCommand[0] + speech.no_one_responds + ' ');
+        } else {
+            buildComment('Go on...');
+        }
     }
 
     function buildComment(text, variable) {
@@ -48,6 +63,14 @@
         } else {
             adminComment = text;
         }
+
+        var currentID = current.sys_id;
+        current.update();
+
+        var gr2 = new GlideRecord('u_404_adventure');
+        gr2.get(currentID);
+        gr2.comments.setJournalEntry(adminComment, 'u_404_dm');
+        gr2.update();
     }
 
     function newCompassDirection(currentDirection, turnDirection) {
